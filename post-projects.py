09f492534ff915de8f54a985222e0f830670d270
root <cbarbieri@vertigo.com.br>
2022-05-14 22:05:57 -0300

    added: post variables with some issues

--- post-projects.py
+++ post-projects.py
@@ -86,9 +86,65 @@ def delete_projects():
                 if response.status_code != 200:
                         print(Fore.RED + str(response))
                 else:
-                        print(Fore.GREEN + str(response))   
+                        print(Fore.GREEN + str(response))
+
+def post_variables():
+        print(Fore.BLUE + '\nPOSTING VARIABLES')
+        
+        variables = []
+        for (dirpath, dirnames, filenames) in walk('./variables'):
+                variables.extend(filenames)
+                variables.sort()
+        
+        projects = []
+        for (dirpath, dirnames, filenames) in walk('./projects'):
+                projects.extend(filenames)
+                projects.sort()
+
+        new_projects = []
+        for (dirpath, dirnames, filenames) in walk('./new-projects'):
+                new_projects.extend(filenames)
+                new_projects.sort()
+        
+        variables_set = set(variables)
+        projects_set = set(projects)
+        variables_set.intersection(projects_set)
+        for i in range(len(variables_set)):
+                        origin_id = variables[i].split("-")
+                        origin_id = origin_id[0]
+                        variable_file = open(f'./variables/{origin_id}-ci_variables.json', 'rb')
+                        variable_data = json.loads(variable_file.read())
+                        
+                        project_path = []
+                        project_id = []
+                        for j in projects:
+                                project_file = open(f'./projects/{j}', 'rb')
+                                project_data = json.loads(project_file.read())
+                                project_path.append(project_data['path'])
+                                project_id.append(str(project_data['id']))
+
+                        new_project_path = []
+                        new_project_id = []
+                        for j in new_projects:
+                                new_project_file = open(f'./new-projects/{j}', 'rb')
+                                new_project_data = json.loads(new_project_file.read())
+                                new_project_path.append(new_project_data['path'])
+                                new_project_id.append(str(new_project_data['id']))
+
+                        for j in range(len(project_path)):
+                                pos = new_project_path.index(project_path[j])
+                                print(project_path[j])
+                                print(pos)
+                                print(new_project_id[pos])
+
+                                for l in range(len(variable_data)):
+                                        print(f'/{new_project_id[pos]}/variables')
+                                        print(variable_data[l])
+                                        response = requests.post(url=api+f'/{new_project_id[pos]}/variables', data=variable_data[l], headers=headers)
+                                        print(response)
 
 if __name__ == "__main__":
-        post()
+        # post()
+        post_variables()
         # delete_projects()
         # push_repo_content()
